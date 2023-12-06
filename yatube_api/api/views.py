429016260ccf3adfd691a521f902e2c424e0c23f
Попкова Дарья <daryapopkova90@yandex.ru>
2023-12-06 15:41:22 +0300

    проверка

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -52,7 +52,7 @@ class FollowViewSet(mixins.CreateModelMixin,
     search_fields = ('following__username',)
 
     def get_queryset(self):
-        return Follow.objects.filter(user=self.request.user)
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
